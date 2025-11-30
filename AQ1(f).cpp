fd635fc9a770dc42a1c9ecdecbc29ed5793ad816
Vaibhav Singhal <vaibhavsinghal0123@gmail.com>
2025-11-30 22:10:52 +0530

    Add files via upload

---
+++ AQ1(f).cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int size = 5;
+    int top = 4; 
+    if (top == size - 1) {
+        cout << "Stack is full";
+    } else {
+        cout << "Stack is not full";
+    }
+
+    return 0;
+}
